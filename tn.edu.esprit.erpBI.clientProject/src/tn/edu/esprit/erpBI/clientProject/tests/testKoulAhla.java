b8b0d279ebcf8659d5ab79786799bd39c5831453
jihene <jihene.ouanes@esprit.tn>
2013-09-19 11:18:33 +0200

    this is my first project

---
+++ tn.edu.esprit.erpBI.clientProject/src/tn/edu/esprit/erpBI/clientProject/tests/testKoulAhla.java
@@ -0,0 +1,20 @@
+package tn.edu.esprit.erpBI.clientProject.tests;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class testKoulAhla {
+	
+	public static void main(String[] args){
+		try {
+			Context context=new InitialContext();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
